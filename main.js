fbe20ef3a3d38481445f5ce58b591ebf8417ac25
Eduardo <langos_cr7@hotmail.com>
2022-12-21 18:31:39 -0600

    fusion del menu mobile

--- main.js
+++ main.js
@@ -1,8 +1,15 @@
 const email = document.querySelector(".navbar-email");
 const menuDesk = document.querySelector(".desktop-menu");
+const btnMenu = document.querySelector(".menu");
+const menuMobile = document.querySelector(".mobile-menu");
 
 email.addEventListener("click", toggleMenu);
+btnMenu.addEventListener("click", toggleMenuMobile);
 
 function toggleMenu() {
     menuDesk.classList.toggle("inactive");
+}
+
+function toggleMenuMobile() {
+    menuMobile.classList.toggle("inactive");
 }
